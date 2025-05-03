a372a72fc63d89f559fc68cf6a836ad49be2fe86
sweetenerbae <diana444435@gmail.com>
2025-05-03 18:17:22 +0300

    fix(onboarding): fix safe area issue

--- TimeTogether/Modules/Onboarding/View/OnboardingView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct OnboardingView: View {
-    var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
-    }
-}
